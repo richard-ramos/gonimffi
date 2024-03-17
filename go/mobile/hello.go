8d3e5356aefa3a6563fa77b4226765ecca303524
Richard Ramos <info@richardramos.me>
2024-03-17 16:09:30 -0400

    build for mobile devices

--- go/mobile/hello.go
+++ go/mobile/hello.go
@@ -3,6 +3,9 @@ package library
 /*
 #include "../libhello.h"
 #cgo amd64,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/x86_64 -lhello
+#cgo arm64,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/arm64-v8a -lhello
+#cgo arm,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/armeabi-v7a -lhello
+#cgo 386,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/x86 -lhello
 */
 import "C"
 
