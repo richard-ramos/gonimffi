29c2455a11d52834cb5b8ac22420fdd4a9713a51
Richard Ramos <info@richardramos.me>
2024-03-18 10:24:20 -0400

    build go for ios

--- go/mobile/hello.go
+++ go/mobile/hello.go
@@ -6,6 +6,9 @@ package library
 #cgo arm64,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/arm64-v8a -lhello
 #cgo arm,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/armeabi-v7a -lhello
 #cgo 386,android LDFLAGS:-L${SRCDIR}/../../nim/build/android/x86 -lhello
+
+#cgo darwin,arm64,ios LDFLAGS:-L${SRCDIR}/../../nim/build/ios/iphone/hello
+#cgo darwin,amd64,iossimulator LDFLAGS:-L${SRCDIR}/../../nim/build/ios/simulator/hello
 */
 import "C"
 
